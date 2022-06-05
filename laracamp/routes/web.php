1e9c97a96f2f33379d1ad2b1ae44fbbf22f5d82c
Rwhytm <rajuwahyudi842@gmail.com>
2022-06-05 11:31:14 +0700

    install breze

--- laracamp/routes/web.php
+++ laracamp/routes/web.php
@@ -17,14 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/checkout', function () {
-    return view('checkout');
-});
-
-Route::get('/success_checkout', function () {
-    return view('success_checkout');
-});
+require __DIR__.'/auth.php';
